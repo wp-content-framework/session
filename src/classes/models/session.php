0c1cd9139f9b295d4e2b8b7ad9009203c008e30d
Technote <technote.space@gmail.com>
2019-05-03 17:21:32 +0900

    wp-content-framework/core#179

--- src/classes/models/session.php
+++ src/classes/models/session.php
@@ -11,6 +11,10 @@
 
 namespace WP_Framework_Session\Classes\Models;
 
+use WP_Framework_Core\Traits\Hook;
+use WP_Framework_Core\Traits\Singleton;
+use WP_Framework_Session\Traits\Package;
+
 if ( ! defined( 'WP_CONTENT_FRAMEWORK' ) ) {
 	exit;
 }
@@ -21,7 +25,7 @@ if ( ! defined( 'WP_CONTENT_FRAMEWORK' ) ) {
  */
 class Session implements \WP_Framework_Core\Interfaces\Singleton, \WP_Framework_Core\Interfaces\Hook {
 
-	use \WP_Framework_Core\Traits\Singleton, \WP_Framework_Core\Traits\Hook, \WP_Framework_Session\Traits\Package;
+	use Singleton, Hook, Package;
 
 	/**
 	 * @var bool $_session_initialized
